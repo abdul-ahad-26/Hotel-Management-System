95e156d52536e5b6d7d001f66ad06eb6f3763a86
abdul-ahad-26 <165171056+abdul-ahad-26@users.noreply.github.com>
2024-07-04 16:58:51 +0500

    Add files via upload

---
+++ WindowsFormsApp3/All User Control/UC_CustomerRegistration.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3.All_User_Control
+{
+    public partial class UC_CustomerRegistration : UserControl
+    {
+        function fn = new function();
+        String query;
+        public UC_CustomerRegistration()
+        {
+            InitializeComponent();
+        }
+
+        public void setComboBox(String query, ComboBox combo)
+        {
+            SqlDataReader sdr = fn.getForCombo(query);
+            while (sdr.Read())
+            {
+                for (int i = 0; i < sdr.FieldCount; i++)
+                {
+                    combo.Items.Add(sdr.GetString(i));
+                }
+            }
+            sdr.Close();
+        }
+
+        private void txtRoom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtRoomNo.Items.Clear();
+            query = "select roomNo from rooms where bed = '" +txtBed.Text+"' and roomType='"+txtRoom.Text+"' and booked = 'NO' ";
+            setComboBox(query, txtRoomNo);
+        }
+        private void txtBed_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtRoom.SelectedIndex = -1;
+            txtRoomNo.Items.Clear();
+            txtPrice.Clear();
+        }
+        int rid;
+        private void txtRoomNo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            query = "select price,roomid from rooms where roomNo = '"+txtRoomNo.Text+"'";
+            DataSet ds = fn.getData(query);
+            txtPrice.Text = ds.Tables[0].Rows[0][0].ToString();
+            rid = int.Parse(ds.Tables[0].Rows[0][1].ToString());
+        }
+
+        private void btnAlloteRoom_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text != "" && txtContact.Text != "" && txtNationality.Text != "" && txtGender.Text != "" && txtDob.Text != "" && txtIdProof.Text != "" && txtAdress.Text != "" && txtCheckIn.Text != "")
+            {
+                String name = txtName.Text;
+                Int64 mobile = Int64.Parse(txtContact.Text);
+                String national = txtNationality.Text;
+                String gender = txtGender.Text;
+                String dob = txtDob.Text;
+                String idproof = txtIdProof.Text;
+                String address = txtAdress.Text;
+                String checkin = txtCheckIn.Text;
+
+                query = "insert into customer(cname,mobile,nationality,gender,dob,idproof,addres,checkin,roomid) values ( '"+name+"',"+mobile+",'"+national+"','"+gender+"','"+dob+"','"+idproof+"','"+address+"','"+checkin+"',"+rid+") update rooms set booked = 'YES' where roomNo = '"+txtRoomNo.Text+"'";
+                fn.setData(query,"Room No " +txtRoomNo.Text+ " Allocation Successful.");
+                clearAll();
+            }
+            else
+            {
+                MessageBox.Show("All fields are mandetory. ", "Information !!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public void clearAll()
+        {
+            txtName.Clear();
+            txtContact.Clear();
+            txtNationality.Clear();
+            txtGender.SelectedIndex = -1;
+            txtDob.ResetText();
+            txtIdProof.Clear();
+            txtAdress.Clear();
+            txtCheckIn.ResetText();
+            txtBed.SelectedIndex = -1;
+            txtRoom.SelectedIndex = -1;
+            txtRoomNo.Items.Clear();
+            txtPrice.Clear();
+        }
+
+        private void UC_CustomerRegistration_Leave(object sender, EventArgs e)
+        {
+            clearAll();
+        }
+    }
+}
---
+++ WindowsFormsApp3/function.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+	class function
+	{
+		protected SqlConnection getConnection()
+		{
+			SqlConnection con = new SqlConnection();
+			con.ConnectionString = "data source=DESKTOP-9C1DA56\\SQLEXPRESS;database=myHotel;integrated security =True";
+			return con;
+		}
+
+		public DataSet getData(String query) // Get data from database
+		{
+			SqlConnection con = getConnection();
+			SqlCommand cmd = new SqlCommand();
+			cmd.Connection = con;
+			cmd.CommandText = query;
+			SqlDataAdapter da = new SqlDataAdapter(cmd);
+			DataSet ds = new DataSet();
+			da.Fill(ds);
+			return ds;
+		}
+
+		public void setData(String query, String message) // Insertion, Deletion, Updation
+		{
+			SqlConnection con = getConnection();
+			SqlCommand cmd = new SqlCommand();
+			cmd.Connection = con;
+			con.Open();
+			cmd.CommandText = query;
+			cmd.ExecuteNonQuery();
+			con.Close();
+			MessageBox.Show("'" + message + "'", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		public SqlDataReader getForCombo(string query)
+		{
+			SqlConnection con = getConnection();
+			SqlCommand cmd = new SqlCommand();
+			cmd.Connection = con;
+			con.Open();
+			cmd = new SqlCommand(query, con);
+			SqlDataReader sdr = cmd.ExecuteReader();
+			return sdr;
+		}
+	}
+}
